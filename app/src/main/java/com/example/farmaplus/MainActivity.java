3bacc7cb5d7d8aef20ec81931b29f296d7b6de88
acarneirod <92722108+acarneirod@users.noreply.github.com>
2022-04-17 17:29:44 +0200

    NavigationDrawer
    A NavigationDrawer has been created manually.
    This contains the options "Inicio", "Farmacias favoritas", "Farmacias cercanas", "Buscador de farmacias" and "Acerca de".
    An empty fragment has been created for each option.
    No graphic resources has been added yet.

--- app/src/main/java/com/example/farmaplus/MainActivity.java
+++ app/src/main/java/com/example/farmaplus/MainActivity.java
@@ -1,14 +1,124 @@
 package com.example.farmaplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
+
+        drawerLayout.addDrawerListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        header.findViewById(R.id.header_title).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, getString(R.string.title_click),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int title;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_inicio:
+                title = R.string.nav_inicio;
+                break;
+            case R.id.nav_favoritas:
+                title = R.string.nav_favoritas;
+                break;
+            case R.id.nav_cercanas:
+                title = R.string.nav_cercanas;
+                break;
+            case R.id.nav_buscador:
+                title = R.string.nav_buscador;
+                break;
+            case R.id.nav_acercade:
+                title = R.string.nav_acercade;
+                break;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        Fragment fragment = InicioFragment.newInstance(getString(title));
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.home_content, fragment)
+                .commit();
+
+        setTitle(getString(title));
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onDrawerSlide(@NonNull View view, float v) {
+        //cambio en la posición del drawer
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View view) {
+        //el drawer se ha abierto completamente
+        Toast.makeText(this, getString(R.string.navigation_drawer_open),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View view) {
+        //el drawer se ha cerrado completamente
+    }
+
+    @Override
+    public void onDrawerStateChanged(int i) {
+        //cambio de estado, puede ser STATE_IDLE, STATE_DRAGGING or STATE_SETTLING
+    }
+
 }
