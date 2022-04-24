9f8ba09cc489d5d988bd3482dc805bad0dd5efa3
acarneirod <92722108+acarneirod@users.noreply.github.com>
2022-04-24 23:38:20 +0200

    RecyclerView-2
    Created FarmaciaDTO with getters and completed the ViewHolder class

---
+++ app/src/main/java/com/example/farmaplus/FarmaciaDTO.java
@@ -0,0 +1,49 @@
+package com.example.farmaplus;
+
+public class FarmaciaDTO {
+    private String id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String horario;
+    private String ayuntamiento;
+    private String cod_provincia;
+
+    public FarmaciaDTO(String id, String nombre, String direccion, String telefono, String horario, String ayuntamiento, String cod_provincia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.horario = horario;
+        this.ayuntamiento = ayuntamiento;
+        this.cod_provincia = cod_provincia;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public String getAyuntamiento() {
+        return ayuntamiento;
+    }
+
+    public String getCod_provincia() {
+        return cod_provincia;
+    }
+}
--- app/src/main/java/com/example/farmaplus/ViewHolder.java
+++ app/src/main/java/com/example/farmaplus/ViewHolder.java
@@ -1,4 +1,20 @@
 package com.example.farmaplus;
 
+import android.widget.TextView;
+
 public class ViewHolder {
+    private TextView nombre;
+    private TextView direccion;
+    private TextView telefono;
+    private TextView horario;
+    private TextView ayuntamiento;
+    private FarmaciaDTO dto;
+
+    public ViewHolder(FarmaciaDTO dto) {
+        this.nombre.setText(dto.getNombre());
+        this.direccion.setText(dto.getDireccion());
+        this.telefono.setText(dto.getTelefono());
+        this.horario.setText(dto.getHorario());
+        this.ayuntamiento.setText(dto.getAyuntamiento());
+    }
 }
