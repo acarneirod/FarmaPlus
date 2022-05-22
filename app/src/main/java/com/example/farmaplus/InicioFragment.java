3c427b53814dbd26a1a1954781bf2e5b4b96bc40
acarneirod <92722108+acarneirod@users.noreply.github.com>
2022-05-22 18:07:53 +0200

    Main screen fragment
    2 RecyclerViews have been added, 1 for the first 2 favorite pharmacies and one for the first 3 nearby pharmacies.

--- app/src/main/java/com/example/farmaplus/InicioFragment.java
+++ app/src/main/java/com/example/farmaplus/InicioFragment.java
@@ -1,26 +1,40 @@
 package com.example.farmaplus;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InicioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
 public class InicioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,7 +42,6 @@ public class InicioFragment extends Fragment {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
     public static InicioFragment newInstance(String param1) {
         InicioFragment fragment = new InicioFragment();
         Bundle args = new Bundle();
@@ -49,7 +62,87 @@ public class InicioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        //FAVORITAS
+        SharedPreferences sharedPref = view.getContext().getSharedPreferences("favoritos", 0);
+        Set<String> hset = new HashSet<>();
+        hset = sharedPref.getStringSet("favoritos", hset);
+        List<FarmaciaDTO> farmacias = new ArrayList<>();
+
+        int cont = 0;
+
+        for (String id : hset){
+            if(cont==2)break;
+            sharedPref = view.getContext().getSharedPreferences(id, 0);
+            Gson gson = new Gson();
+            String json = sharedPref.getString(id, "");
+            FarmaciaDTO dto = gson.fromJson(json, FarmaciaDTO.class);
+            farmacias.add(dto);
+            cont++;
+        }
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(farmacias);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerPrincipalFavoritas);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(),DividerItemDecoration.VERTICAL));
+
+
+
+        //CERCANAS
+        String url = "https://625058afe3e5d24b3420b189.mockapi.io/farmacia?search=" + codigoProvinciaGeolocalizado();
+        JsonArrayRequest peticion = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        List<FarmaciaDTO> farmacias = new ArrayList<>();
+                        for(int i=0;i<3;i++){
+                            try{
+                                JSONObject farmacia = response.getJSONObject(i);
+                                FarmaciaDTO farmaciaDTO = new FarmaciaDTO(farmacia.getString("nombre"),
+                                        farmacia.getString("direccion"),
+                                        farmacia.getString("telefono"),
+                                        farmacia.getString("horario"),
+                                        farmacia.getString("ayuntamiento"),
+                                        farmacia.getString("cod_provincia"),
+                                        farmacia.getString("id"));
+                                farmacias.add(farmaciaDTO);
+                            }catch(JSONException e){
+                                e.printStackTrace();
+                            }
+                        }
+                        RecyclerViewAdapter adapter = new RecyclerViewAdapter(farmacias);
+                        RecyclerView recyclerView = view.findViewById(R.id.recyclerPrincipalCercanas);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+                        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(),DividerItemDecoration.VERTICAL));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue cola = Volley.newRequestQueue(view.getContext());
+        cola.add(peticion);
+
+
+
+
+
+
+        return view;
     }
+
+    private String codigoProvinciaGeolocalizado(){
+        //TODO
+        return "ES-C";
+    }
+
 }
